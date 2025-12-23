8df1430805d33f2a1a19ab3a09402182352fe7c9
Reyansh <mantareyansh@gmail.com>
2025-12-23 16:49:03 +0530

    minor changes

--- app/components/CallForVolunteers.tsx
+++ app/components/CallForVolunteers.tsx
@@ -1,6 +1,6 @@
 export default function CallForVolunteers() {
     return (
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfRlbtegrypHBHw8sHVy_FEcIYLGBo6i5ZETeLxY6NFfSnYjw/viewform">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdByt9-Cb7vV2ceZnLMcbOSUBA4YfFKMp17QxJAesiddUYpdA/viewform">
         <div className="md:col-span-1 md:row-span-2 rounded-bento p-8 border border-dark-border bento-card flex flex-col relative overflow-hidden group" style={{ background: 'var(--gradient-card)' }}>
             <div className='relative z-20'>
 
